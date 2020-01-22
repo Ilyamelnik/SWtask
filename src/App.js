fc8ced085fc7a8f88fc8b99fdc34b92a24f6621a
ilyamelnik <iliamelnik@outlook.com>
2020-01-22 16:10:37 +0300

    added server and implemented service worker

--- src/App.js
+++ src/App.js
@@ -2,23 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     }).catch(function (err) {
+//       console.log(err)
+//     });
+//   });
+// } else {
+//   console.log('service worker is not supported');
+// }
+
+const check = () => {
+  if (!('serviceWorker' in navigator)) {
+    throw new Error('No Service Worker support!')
+  }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
+}
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+  console.log('swRegistration', swRegistration);
+  showLocalNotification('This is title', 'this is the message', swRegistration);
+}
+
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register('sw.js', { scope: '/' })
+  return swRegistration;
+}
+
+const showLocalNotification = (title, body, swRegistration) => {
+  const options = {
+    body,
+  };
+  swRegistration.showNotification(title, options);
+}
+
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  console.log('permission', permission);
+  if (permission !== 'granted') {
+    throw new Error('Permission not granted for Notification');
+  }
+}
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <button id="permission-btn" onClick={main}>Ask Permission</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+// const check = () => {
+//   if (!('serviceWorker' in navigator)) {
+//     throw new Error('No Service Worker support!')
+//   }
+//   if (!('PushManager' in window)) {
+//     throw new Error('No Push API Support!')
+//   }
+// }
+// const main = async () => {
+//   check();
+//   const swRegistration = await registerServiceWorker();
+//   const permission = await requestNotificationPermission();
+//   console.log('swRegistration', swRegistration);
+//   showLocalNotification('This is title', 'this is the message', swRegistration);
+// }
+
+// const registerServiceWorker = async () => {
+//   const swRegistration = await navigator.serviceWorker.register('/src/sw.js')
+//   return swRegistration;
+// }
+
+// const showLocalNotification = (title, body, swRegistration) => {
+//   const options = {
+//     body,
+//   };
+//   swRegistration.showNotification(title, options);
+// }
+
+// const requestNotificationPermission = async () => {
+//   const permission = await window.Notification.requestPermission();
+//   console.log('permission', permission);
+//   if (permission !== 'granted') {
+//     throw new Error('Permission not granted for Notification');
+//   }
+// }
+
+
+
+// main();
+
